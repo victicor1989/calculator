793fc7f68ee6c9ba6a797e8f0bfdbffd313ff3a6
victicor1989 <losdelsur1947@hotmail.com>
2023-02-14 12:28:40 -0500

    v1.3 se termina la calculadora con HTML, CSS y JS

--- Display.js
+++ Display.js
@@ -1,11 +1,17 @@
 class Display {
     constructor(displayValorAnterior, displayValorActual) {
         this.displayValorActual = displayValorActual;
-        this.displayValorAnterior = displayValorAnterior
+        this.displayValorAnterior = displayValorAnterior;
         this.calculadora = new Calculadora();
         this.tipoDeOperacion  = undefined;
         this.valorActual = '';
         this.valorAnterior = '';
+        this.signos = {
+            sumar: '+',
+            dividir: '%',
+            multiplicar: 'x',
+            restar: '-'
+        }
     }
 
     borrar() {
@@ -20,14 +26,30 @@ class Display {
         this.imprimirValores();
     }
 
+    computar(tipo) {
+        this.tipoDeOperacion !== 'igual' && this.calcular();
+        this.tipoDeOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
+
     agregarNumero(numero) {
         if (numero === '.' && this.valorActual.includes('.')) return
-        this.valorActual =  this.valorActual.toString + numero;
+        this.valorActual =  this.valorActual.toString() + numero;
         this.imprimirValores();
     }
 
     imprimirValores() {
         this.displayValorActual.textContent = this.valorActual;
-        this.displayValorAnterior.textContent = this.valorAnterior;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoDeOperacion] || ''}`;
+    }
+
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if (isNaN(valorActual) || isNaN(valorAnterior) ) return;
+        this.valorActual = this.calculadora[this.tipoDeOperacion](valorAnterior, valorActual)
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const displayValorAnterior = document.querySelector('#valor-anterior');
 const displayValorActual = document.querySelector('#valor-actual');
 const botonesNumeros = document.querySelectorAll('.numero');
-const botonesOperadores = document.querySelectorAll('operador');
+const botonesOperadores = document.querySelectorAll('.operador');
 
 const display = new Display(displayValorAnterior, displayValorActual);
 
 botonesNumeros .forEach(boton => {
     boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
 });
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+})
